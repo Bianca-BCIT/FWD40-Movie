8810b0c7a9fb539c7b0fbf02088b48cfe6136e86
Arash Laali <arashlaali@Arashs-MacBook-Pro.local>
2025-02-08 09:08:53 +0330

    in .movieCard added FavoriteButton , and added new function to navigate us to the url on the click

---
+++ src/components/FavoriteButton.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import "./FavoriteButton.css";
+import { GlobalContext } from "../context/GlobalContext";
+
+function FavoriteButton(movieData) {
+    const { favorites, addFavorite, removeFavorite } = useContext(GlobalContext);
+    //check if movie already in favorites 
+    const isFavorite = favorites.find((fav) => fav.id === movieData.id);
+
+
+    function handleFavorite(event) {
+        // will prevent click from bubbling up to the parent and causing link to be blicked and redirecting to movie page 
+        event.stopPropagation();
+        //like a toggle switch 
+        // if it is , clicling the the button should remove it from fav, if not ,should add it. 
+        if (isFavorite) {
+            removeFavorite(movieData);
+        } else {
+            addFavorite(movieData);
+        }
+    }
+
+    // the fav button need to know whether the current movie it is connected to, its already in favorites or not.
+
+    return <button onClick={handleFavorite}>{isFavorite ? "🤍" : "❤️"}</button>
+}
+
+
+export default FavoriteButton;
+
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,32 @@
 import { TMDB_IMAGE_BASE_URL } from "../globals/globals";
 import "./MovieCard.css";
 import { formatReleaseDate, formatVoteAverage } from "../utilities/toolbelt";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import FavoriteButton from "./FavoriteButton";
 
-function MovieCard({movieData}) {
+function MovieCard({ movieData }) {
+    const navigate = useNavigate();
+    function handleLinkClick(event) {
+        navigate(`/movie/${movieData.id}`);
+
+    }
+
     return (
-        <Link to={`/movie/${movieData.id}`}>
-            <div className="movie-card">
-                <img src={`${TMDB_IMAGE_BASE_URL}/w300/${movieData.poster_path}`}
-                alt={`${movieData.title} poster`}/>
-                <div className="movie-card-info">
-                    <h2 className="movie-title">{movieData.title}</h2>
-                    <p className="release-date">{formatReleaseDate(movieData.release_date)}</p>
-                    <p className="overview">{movieData.overview}</p>
-                    <p className="vote-average">{formatVoteAverage(movieData.vote_average)}</p>
-                    <button className="see-more">See More</button>
-                </div>
-                <button className="favorite">🩷</button>
+        <div onClick={handleLinkClick} className="movie-card">
+            {/* // <Link to={`/movie/${movieData.id}`}> */}
+
+            <img src={`${TMDB_IMAGE_BASE_URL}/w300/${movieData.poster_path}`}
+                alt={`${movieData.title} poster`} />
+            <div className="movie-card-info">
+                <h2 className="movie-title">{movieData.title}</h2>
+                <p className="release-date">{formatReleaseDate(movieData.release_date)}</p>
+                <p className="overview">{movieData.overview}</p>
+                <p className="vote-average">{formatVoteAverage(movieData.vote_average)}</p>
+                <button className="see-more">See More</button>
             </div>
-        </Link>
+            <FavoriteButton movieData={movieData} />
+        </div>
+        // </Link >
     );
 }
 
