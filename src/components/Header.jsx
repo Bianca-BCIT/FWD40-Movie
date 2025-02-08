5652a952907b79645ca46dc386e3d29778f17155
Arash Laali <arashlaali@Arashs-MacBook-Pro.local>
2025-02-08 10:02:23 +0330

    in pagefavorite added a function to retrieve data from global contect and map over them to display movie cards , and added css for it , as well added cursor :pointer for the moviecard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 
 import Banner from '../images/Banner.png';
 import '../components/Header.css';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
     return <header>
+        <nav>
+            <ul>
+                <li>
+                    <link to="/">Home</link>
+                </li>
+                <li>
+                    <link to="/favorites">Favorites</link>
+                </li>
+            </ul>
+        </nav>
         <img className="banner" src={Banner} alt="White Bird Backdrop" />
     </header>
 }
---
+++ src/pages/pageFavorites.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import "./PageFavorites.css";
+import { GlobalContext } from "../context/GlobalContext";
+import MovieCard from "../components/MovieCard";
+
+function PageFavorites() {
+
+    // retrieve our favorites from global context
+
+    const { favorites } = useContext(GlobalContext);
+
+    return <main id="favorites">
+        <h2>Favorites</h2>
+        {/* map over them and display movie cards */}
+        <div className="favorites-container">
+            {favorites.map((movie) => {
+
+                return <MovieCard key={movie.id} movieData={movie} />;
+
+            })}
+        </div>
+
+    </main>
+
+
+}
+
+export default PageFavorites; 
